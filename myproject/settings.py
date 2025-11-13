fd36255811771dfcb8646d00bc1589709740c1af
taoufik03-creator <taoufikbouhamla6@gmail.com>
2025-11-13 22:59:54 +0100

    Fix ALLOWED_HOSTS for Railway

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%z67t=lwce2idzz6=od#hnj31s9v7)@q+3af#d376jznnfl=yx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-app.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-1c6ee.up.railway.app','your-app.up.railway.app', 'localhost', '127.0.0.1']
 
 
 
